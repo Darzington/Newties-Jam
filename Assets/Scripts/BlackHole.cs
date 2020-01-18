efe7b5b0fa7cd470b1601fa05f57e6256385cf05
Miled Chalal-Henri <miledch@gmail.com>
2020-01-18 13:05:04 -0500

    second commit godamit

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Animator;
+
+public class BlackHole : MonoBehaviour
+{
+    [SerializeField]
+    bool spin0;
+    [SerializeField]
+    bool spin1;
+    [SerializeField]
+    bool spin2;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(spin0)
+
+        
+    }
+}
---
+++ Assets/Scripts/GameEventManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameEventManager
+{
+    private static GameEventManager instance;
+
+    public static GameEventManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEventManager();
+            }
+            return instance;
+        }
+    }
+
+    // Private ctor ... Singleton!
+    private GameEventManager()
+    {
+    }
+    
+    public Action OnBlackHoleSizeUp;
+    public Action OnBlackHoleSizeDown;
+    
+}
