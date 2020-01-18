d82752c5845ebba1e07cc79229162c084aaa3542
Nicole Parmentier <nclparmentier@gmail.com>
2020-01-18 07:47:16 -0500

    Custom cursor size management

--- Assets/Scripts/PointNShoot.cs
+++ Assets/Scripts/PointNShoot.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class PointNShoot : MonoBehaviour
 {
-    public Texture2D reticule;
-    public GameObject laserPrefab;
-    public Camera orthoCam;
+    [SerializeField] private Texture2D reticule;
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private Camera cam;
 
-    // Start is called before the first frame update
+    private Vector2 mousePosition = Vector2.zero;
+    private float cursorIconWidth, cursorIconHeight;
+
     void Start()
     {
-        Cursor.SetCursor(reticule, new Vector2(reticule.width/2.0f, reticule.height/2.0f), CursorMode.Auto);
+        Cursor.visible = false;
+        cursorIconHeight = reticule.height;
+        cursorIconWidth = reticule.width;
+    }
+       
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(mousePosition.x - (cursorIconWidth / 2), mousePosition.y - (cursorIconHeight / 2), cursorIconWidth, cursorIconHeight), reticule);
     }
 
     void Update()
     {
+        mousePosition = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+
         if (Input.GetButtonDown("Fire1"))
         {
-            Ray ray = orthoCam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit RayHit;
             if (Physics.Raycast(ray, out RayHit))
             {
                 GameObject laser = Instantiate(laserPrefab);
-                laser.transform.position = orthoCam.transform.position + new Vector3(0, 0, 1);
+                laser.transform.position = cam.transform.position + new Vector3(0, 0, 1);
 
                 Vector3 targetPos = RayHit.point;
                 LaserProjectile lp = laser.GetComponent<LaserProjectile>();
                 lp.SetTarget(targetPos);
 
-                Vector3 direction = targetPos - orthoCam.transform.position;
+                Vector3 direction = targetPos - cam.transform.position;
                 Vector3 startRotation = laser.transform.eulerAngles;
                 laser.transform.rotation = Quaternion.LookRotation(direction);
                 laser.transform.eulerAngles += startRotation;
